e1b27fb5b9867b8afe8e74ffdf2e590bb82cf3ef
horamupa <horamupa@gmail.com>
2022-09-07 14:33:11 +0300

    Initial Commit

---
+++ MindMapApp/MindMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMapAppApp.swift
+//  MindMapApp
+//
+//  Created by MM on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MindMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
